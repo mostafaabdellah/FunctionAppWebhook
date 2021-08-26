26b5cf3685a4045e91dd915a45b6702a3cafb6de
moustafa <mostafa.abdellah@hotmail.com>
2021-08-26 13:54:49 -0400

    Add project files.

---
+++ FunctionAppWebhook/Notifications.cs
@@ -0,0 +1,40 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace FunctionAppWebhook
+{
+    public static class Notifications
+    {
+        [FunctionName("Notifications")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            try
+            {
+                var storageConnection = string.Empty;
+                storageConnection = System.Environment.GetEnvironmentVariable("AzureWebJobsStorage", EnvironmentVariableTarget.Process);
+                StorageAccount storageAccount = StorageAccount.NewFromConnectionString(storageConnection);
+                var queueClient = storageAccount.CreateCloudQueueClient();
+                var queue = queueClient.GetQueueReference("webhooknotifications");
+                queue.CreateIfNotExistsAsync();
+                queue.AddMessageAsync(new Microsoft.Azure.Storage.Queue.CloudQueueMessage(requestBody));
+
+            }
+            catch (Exception exc)
+            {
+
+                log.LogError(exc.Message);
+            }
+            return new OkResult();
+        }
+    }
+}
